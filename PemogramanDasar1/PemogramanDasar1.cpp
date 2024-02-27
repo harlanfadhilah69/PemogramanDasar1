dc8c351224555756ab7ef3669d266a906372c93b
harlanfadhilah69 <161017962+harlanfadhilah69@users.noreply.github.com>
2024-02-27 18:49:53 +0700

    Add project files.

---
+++ PemogramanDasar1/PemogramanDasar1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{//begin
+
+    // numeric nJejari, nLuas
+    // display 'masukan jejari= '
+    // accept nJejari
+    // compute nLuas = 3.14 * r * r
+    // display 'Luasnya= ' + nLuas
+    float nJejari, nLuas;
+
+    cout << "masukan Jejari";
+    cin >> nJejari;
+    nLuas = 3.14 * nJejari * nJejari;
+    cout << "Luas lingkaran adalah = " << nLuas << endl;
+
+    system("pause");
+
+    
+}//end
+
